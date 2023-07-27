57d60c9c642b31ad805b0dc86032e4691ac18944
Alexander Shilenkov <alexesc154@gmail.com>
2023-07-27 23:02:14 +0300

    add SignUpScreen

---
+++ social_network_chat/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:social_network_chat/pages/sign_up_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: SignUpScreen(),
+    );
+  }
+}
+
